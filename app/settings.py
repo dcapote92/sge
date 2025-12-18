538012e1772f19096222c6df0840fddde148efe2
dcapote92 <danicapote92@gmail.com>
2025-12-18 20:42:45 -0300

    all apps added on main settings

--- app/settings.py
+++ app/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'brands',
+    'categories',
+    'suppliers',
+    'products',
+    'inflows',
+    'outflows',
+
 ]
 
 MIDDLEWARE = [
