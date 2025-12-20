1681007d5523a8c6dfdd22eadebea76781aeafed
dcapote92 <danicapote92@gmail.com>
2025-12-19 22:34:25 -0300

    brands app fully setted up

--- brands/admin.py
+++ brands/admin.py
@@ -4,4 +4,5 @@ from brands.models import Brand
 
 @admin.register(Brand)
 class BrandAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'created_at', 'updated_at')
+    list_display = ('id', 'name', 'description', 'created_at', 'updated_at')
+    search_fields = ('name',)
