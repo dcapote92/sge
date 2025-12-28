a930a557c32b067dc3fd8a643a4c78cc4725c024
dcapote92 <danicapote92@gmail.com>
2025-12-28 13:53:16 -0300

    product metrics bug fixed, settings changed to use posgresql instead sqlite

--- app/metrics.py
+++ app/metrics.py
@@ -1,33 +1,45 @@
-from django.db.models import Sum, F
+from django.db.models import F, Sum
 from django.utils import timezone
 from django.utils.formats import number_format
-from products.models import Product
-from outflows.models import Outflow
-from categories.models import Category
+
 from brands.models import Brand
+from categories.models import Category
+from outflows.models import Outflow
+from products.models import Product
 
 
 def get_product_metrics():
     products = Product.objects.all()
     total_quantity = sum(product.quantity for product in products)
-    total_cost_price = sum(product.cost_price for product in products)
-    total_selling_price = sum(product.selling_price for product in products)
+    total_cost_price = sum(
+        product.cost_price * product.quantity for product in products
+    )
+    total_selling_price = sum(
+        product.selling_price * product.quantity for product in products
+    )
     total_profit = total_selling_price - total_cost_price
 
     return dict(
         total_quantity=total_quantity,
-        total_cost_price=number_format(total_cost_price, decimal_pos=2, force_grouping=True),
-        total_selling_price=number_format(total_selling_price, decimal_pos=2, force_grouping=True),
-        total_profit=number_format(total_profit, decimal_pos=2, force_grouping=True)
+        total_cost_price=number_format(
+            total_cost_price, decimal_pos=2, force_grouping=True
+        ),
+        total_selling_price=number_format(
+            total_selling_price, decimal_pos=2, force_grouping=True
+        ),
+        total_profit=number_format(total_profit, decimal_pos=2, force_grouping=True),
     )
 
 
 def get_sales_metrics():
     outflows = Outflow.objects.all()
     total_sales = Outflow.objects.count()
-    total_products_sold = Outflow.objects.aggregate(
-        total_products_sold=Sum('quantity')
-    )['total_products_sold'] or 0
+    total_products_sold = (
+        Outflow.objects.aggregate(total_products_sold=Sum("quantity"))[
+            "total_products_sold"
+        ]
+        or 0
+    )
     total_sales_value = sum(outflow.product.selling_price for outflow in outflows)
     total_sales_cost = sum(outflow.product.cost_price for outflow in outflows)
     total_sales_profit = total_sales_value - total_sales_cost or 0
@@ -35,8 +47,12 @@ def get_sales_metrics():
     return dict(
         total_sales=total_sales,
         total_products_sold=total_products_sold,
-        total_sales_value=number_format(total_sales_value, decimal_pos=2, force_grouping=True),
-        total_sales_profit=number_format(total_sales_profit, decimal_pos=2, force_grouping=True)
+        total_sales_value=number_format(
+            total_sales_value, decimal_pos=2, force_grouping=True
+        ),
+        total_sales_profit=number_format(
+            total_sales_profit, decimal_pos=2, force_grouping=True
+        ),
     )
 
 
@@ -45,18 +61,15 @@ def get_daily_sales_data():
     dates = [str(today - timezone.timedelta(days=i)) for i in range(6, -1, -1)]
     values = [
         float(
-            Outflow.objects.filter(created_at__date=date)
-            .aggregate(total_sales=Sum(
-                F('product__selling_price') * F('quantity')
-            )
-            )['total_sales'] or 0
-        ) for date in dates
+            Outflow.objects.filter(created_at__date=date).aggregate(
+                total_sales=Sum(F("product__selling_price") * F("quantity"))
+            )["total_sales"]
+            or 0
+        )
+        for date in dates
     ]
 
-    return dict(
-        dates=dates,
-        values=values
-    )
+    return dict(dates=dates, values=values)
 
 
 def get_daily_sales_quantity_data():
@@ -66,21 +79,17 @@ def get_daily_sales_quantity_data():
         Outflow.objects.filter(created_at__date=date).count() for date in dates
     ]
 
-    return dict(
-        dates=dates,
-        values=quantities
-    )
+    return dict(dates=dates, values=quantities)
 
 
 def get_graphic_product_category_metric():
     categories = Category.objects.all()
     return {
-        category.name: Product.objects.filter(category=category).count() for category in categories
+        category.name: Product.objects.filter(category=category).count()
+        for category in categories
     }
 
 
 def get_graphic_product_brand_metric():
     brands = Brand.objects.all()
-    return {
-        brand.name: Product.objects.filter(brand=brand).count() for brand in brands
-    }
+    return {brand.name: Product.objects.filter(brand=brand).count() for brand in brands}
